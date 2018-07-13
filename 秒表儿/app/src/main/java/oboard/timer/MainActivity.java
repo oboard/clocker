33d8f048a0c51fe53259c4bea01522ec2c5c857a
oboard <oboard@outlook.com>
2018-07-13 19:07:49 +0800

    添加启动说明

--- 秒表儿/app/src/main/java/oboard/timer/MainActivity.java
+++ 秒表儿/app/src/main/java/oboard/timer/MainActivity.java
@@ -1,50 +1,47 @@
 package oboard.timer;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
+import oboard.timer.MainActivity;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
     public static ClockSurfaceView local;
     Handler handler = new Handler();
     public static boolean paused = true;
     public static long time = 0, time2 = 0;
-    private void Pause()
-    {
+    private void Pause() {
         this.paused = true;
     }
 
-    private void Start()
-    {
+    private void Start() {
         this.paused = false;
         this.handler.post(this.runnable);
         if (this.time == 0)
             this.time2 = System.currentTimeMillis();
     }
 
-    private void Stop()
-    {
+    private void Stop() {
         this.Pause();
         this.time = 0;
-       
+
         local.fresh();
     }
 
     @Override
-    protected void onCreate(Bundle bundle)
-    {
+    protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         this.setContentView(R.layout.activitymain);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);            //竖屏
-        if (Build.VERSION.SDK_INT >= 19)
-        {
+        if (Build.VERSION.SDK_INT >= 19) {
             WindowManager.LayoutParams layoutParams = this.getWindow().getAttributes();
             layoutParams.flags = 67108864 | layoutParams.flags;
         }
@@ -52,32 +49,51 @@ public class MainActivity extends Activity
         local.setClickable(true);
         local.setOnClickListener(new View.OnClickListener() {
 
-                public void onClick(View view)
-                {
-                    if (paused)
-                    {
+                public void onClick(View view) {
+                    if (paused) {
                         Start();
                         return;
                     }
                     Pause();
                 }
             });
-         local.setLongClickable(true);
-         local.setOnLongClickListener(new View.OnLongClickListener() {
-             @Override
-             public boolean onLongClick(View view) {
-                 Stop();
-                 return true;
-             }
-         });
-         //local.setDrawingCacheEnabled(true);
-    }
-    
-    Runnable runnable = new Runnable() {
+        local.setLongClickable(true);
+        local.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    if (paused)
+                        Stop();
+                    return true;
+                }
+            });
+        //local.setDrawingCacheEnabled(true);
 
+
+        SharedPreferences shared = getSharedPreferences("is", MODE_PRIVATE);
+        SharedPreferences.Editor editor = shared.edit();
+        if (shared.getBoolean("isfer", true)) {
+            //首次启动
+            new AlertDialog.Builder(MainActivity.this)
+                //    设置Title
+                .setTitle("说明")
+                //    设置Content来显示一个信息
+                .setMessage("单击/n开始/暂停/n/n长按/n清零")
+                //    设置一个PositiveButton
+                .setPositiveButton("知道啦！", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "positive: " + which, Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
+            editor.putBoolean("isfer", false)
+                .commit();
+        }
+    }
+
+    Runnable runnable = new Runnable() {
         @Override
-        public void run()
-        {
+        public void run() {
             if (paused)
                 return;
             handler.post(this);
